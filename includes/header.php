1e151acb9c36f71ae6dd1e8011083f1d58d2c8ed
Skyll_Box <113237888+Damien-Codes@users.noreply.github.com>
2023-11-15 16:25:32 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
                             <h1>DM-Articles</h1>
-                            <span class="subheading">Make by Maxence .</span>
+                            <span class="subheading">Make by PINO Damien .</span>
                         </div>
                     </div>
                 </div>
